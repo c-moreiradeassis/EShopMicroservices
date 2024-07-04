b8b105774a6e2067a4bde61d1d5a9f9c44b3217d
Carlos Assis <c.moreiradeassis@gmail.com>
2024-07-04 15:18:11 -0300

    feat: final version.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,33 @@
+﻿using Marten.Schema;
+using System.Xml;
+
+namespace Catalog.API.Data
+{
+    public class CatalogInitialData : IInitialData
+    {
+        public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+        {
+            var session = store.LightweightSession();
+
+            if (await session.Query<Product>().AnyAsync())
+                return;
+
+            session.Store<Product>(GetPreconfiguredProducts());
+
+            await session.SaveChangesAsync();
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts() => new List<Product>()
+        {
+            new Product()
+            {
+                Id = new Guid("f37b7520-a063-4e81-8332-e1e84a6e249f"),
+                Name = "IPhone X",
+                Description = "This phone is the best one ever.",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category = new List<string>{"Smart Phone"}
+            }
+        };
+    }
+}
