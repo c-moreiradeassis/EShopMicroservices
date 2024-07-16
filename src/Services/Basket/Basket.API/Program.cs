e46926af074c0fbaca43f3828d872f240d6c669c
Carlos Assis <c.moreiradeassis@gmail.com>
2024-07-16 11:56:17 -0300

    feat: basket-api created.

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,32 @@
+
+using BuildingBlocks.Exceptions.Handler;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.Run();
